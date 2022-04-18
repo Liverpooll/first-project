f258e76a581af26436c8a4f7a838ed80029ad14f
jisu <jisu@jisuui-MacBookPro.local>
2022-04-18 13:28:56 +0900

    [Create] async-test function

--- todo/todos/urls.py
+++ todo/todos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from todos.views import TodoListView
-
+from todos import views
 
 app_name = 'todos'
 
 urlpatterns = [
     path('todolist/', TodoListView.as_view(), name='todolist'),
+    path('async-test/', views.async_test, name='async-test'),
 ]
