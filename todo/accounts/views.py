d46b031801717dc8160b27c5930f07c70b3aea46
jisu <jisu@jisuui-MacBookPro.local>
2022-04-26 19:13:32 +0900

    [Create] user class, implement signup

--- todo/accounts/views.py
+++ todo/accounts/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
 
+from django.views import View
+
+from accounts.models import User
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 
 def login(request):
     return render(request, 'login.html')
+
+
+class LoginView(View):
+    template_name = 'login.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+def logout(request):
+    pass
+
+
+class SignupView(View):
+    template_name = 'signup.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+    def post(self, request, *args, **kwargs):
+        user = User.objects.create_user(
+            request.POST.get('email'),
+            request.POST.get('pw'),
+        )
+        user.nickname = request.POST.get('nickname')
+        return render(request, self.template_name)
