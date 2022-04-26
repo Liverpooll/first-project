9d0bc44043dcf29d364775850c0f481cd00ee3ec
jisu <jisu@jisuui-MacBookPro.local>
2022-04-26 19:26:50 +0900

    [Add] logout

--- todo/accounts/views.py
+++ todo/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 from django.views import View
 
@@ -19,7 +19,7 @@ class LoginView(View):
 
 
 def logout(request):
-    pass
+    logout(request)
 
 
 class SignupView(View):
