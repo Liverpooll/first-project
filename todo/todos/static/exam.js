59b03cad51b350e10a2218b6903536262aeb5bac
Eliott <fbgus333@naver.com>
2022-05-11 22:44:09 +0900

    Toda I updated

--- todo/todos/static/exam.js
+++ todo/todos/static/exam.js
@@ -1,9 +1,10 @@
 'use strict';
 
-let create = document.querySelector('.btn');
-let edit = document.querySelector('.btn_edit');
-let del = document.querySelector('.btn_delete');
+const form_box = document.querySelector('.form-box');
+const todo_input = document.querySelector('.todoValue');
+const submit_btn = document.querySelector('.btn-outline-dark');
 const inner_box = document.querySelector('.Down-box1');
+
 
 function getCsrfToken() {
     return document.cookie.split(';')
@@ -18,14 +19,13 @@ document.querySelector('.todoValue').addEventListener('keyup', (e, event) => {
 });
 
 
-function create_todo() {
+function create_todo(id) {
     // 1. 적은 값의 정보를 받기
     // 2. 받은 정보를 비동기 통신으로 데이터 저장하게 하기.
     // 3. 비동기 통신이 성공하면 노드 추가하기
     // 4. 실패하면 추가 안함
-    let todo_value = $(".todoValue").val();
 
-    if (todo_value == '') {
+    if (todo_input.value == '') {
         alert('뭐라도 입력해봐');
     } else {
         axios({
@@ -36,7 +36,7 @@ function create_todo() {
                     'X-CSRFToken': getCsrfToken(),
                 },
                 data: {
-                    'todo': todo_value,
+                    'todo': todo_input.value,
                 },
             })
             .then(function(response) {
@@ -44,34 +44,37 @@ function create_todo() {
                 let id = response.data['id'];
 
                 if (result == true) {
-                    const node_list = document.querySelectorAll('.todo_list_li');
-                    const li = document.createElement("li");
-                    const button = document.createElement("button");
-                    const button_1 = document.createElement("button");
-                    const div = document.createElement("div");
+                    let li = document.createElement('li');
+                    let span = document.createElement('span');
+                    let M_button = document.createElement('button');
+                    let X_button = document.createElement('button');
+                    let radio = document.createElement('input');
+
+
+                    radio.type = 'radio';
+                    radio.setAttribute('onclick', `done_onclick(${id},event)`);
+                    radio.classList.add('btn_done');
+
+                    span.innerText = todo_input.value;
+                    span.style.cursor="pointer"
+                    span.setAttribute("onclick", `change_input_onclick(${id}, event)`);
                     li.classList.add('todo_list_li');
-                    div.id = node_list.length;
-                    div.classList.add('todo_list_div');
-                    div.innerHTML = todo_value;
-                    button.innerHTML = 'M';
-                    button.setAttribute("onclick", `change_input_onclick(${id}, event)`);
-                    button.classList.add('btn_edit');
-                    button.value = node_list.length;
 
-                    button_1.innerHTML = 'X';
-                    button_1.setAttribute("onclick", `delete_onclick(${id}, event)`);
-                    button_1.classList.add('btn_delete');
-                    button_1.value = node_list.length;
+                    M_button.innerText = 'M'
+                    M_button.setAttribute("onclick", `change_input_onclick(${id}, event)`);
+                    M_button.classList.add('btn_edit');
 
-                    li.appendChild(div);
-                
-                    li.appendChild(button);
-                    li.appendChild(button_1);
+                    X_button.innerText = 'X';
+                    X_button.setAttribute("onclick", `delete_onclick(${id}, event)`);
+                    X_button.classList.add('btn_delete');
+
+                    li.appendChild(radio);
+                    li.appendChild(span);
+                    li.appendChild(X_button);
+
                     inner_box.appendChild(li);
 
-
-                    document.querySelector(".todoValue").value = null;
-
+                    todo_input.value = null;
 
 
                 } else {
@@ -84,17 +87,44 @@ function create_todo() {
     }
 }
 
-let test_event = ''
 
 // 체인지 인풋 온클릭 함수에서 누는 수정완료 버튼이 클릭 됐을 때 연결되는 함수
 function edit_success_onclick(id, event) {
-    test_event = event;
+    let event_target = event.target.value;
+    console.log(event_target)
+    let new_input = document.querySelector('.new_input');
+    let event_parent = event.target.parentElement;
+    let firstNode = event_parent.children[0];
+    let secondNode = event_parent.children[1];
+    let thirdNode = event_parent.children[2];
+    let M_button = document.createElement('button');
+    let span = document.createElement('span');
+    let radio = document.createElement('input');
+
+
+    radio.type = 'radio';
+    radio.setAttribute('onclick', `done_onclick(${id},event)`);
+    radio.classList.add('btn_done');
+
+
+    span.innerText = new_input.value;
+
+    span.style.cursor="pointer"
+    span.setAttribute("onclick", `change_input_onclick(${id}, event)`);
+
+
+    M_button.innerText = 'M'
+    M_button.setAttribute("onclick", `change_input_onclick(${id}, event)`);
+    M_button.classList.add('btn_edit');
+
+
+
+
     // 1. 해당 노드의 정보 받기
     // 2. 비동기 통신 보내기.
     // 3. 성공하면 크리에이트 함수 동작 할 때의 노드 상태와 똑같이 바꾸기.
     // 4. 실패하면 안되게 하기
-    const node_id = event.target.value;
-    const content = document.querySelector('.new_input').value;
+
     axios({
             url: `/todos/edit/`,
             method: 'post',
@@ -103,7 +133,7 @@ function edit_success_onclick(id, event) {
                 'X-CSRFToken': getCsrfToken(),
             },
             data: {
-                'todo': content,
+                'todo': new_input.value,
                 'id': id,
             },
         })
@@ -111,28 +141,9 @@ function edit_success_onclick(id, event) {
             let result = response.data['is_success']
             if (result === true) {
 
-                const button_2 = document.querySelectorAll('.new_button')[node_id]
-                const list_id = document.querySelectorAll('.todo_list_div')[node_id];
-                // const todo_list1 = document.querySelectorAll('.todo_list')[node_id];
-                const todo_list = document.querySelectorAll('.todo_list')[node_id];
-                const node_list = document.querySelector('.todo_list_div');
-                const button = document.createElement("button");
-                const new_input = document.querySelector('.new_input');
-                const button_1 = document.querySelectorAll('.btn_delete')[node_id];
-                const div = document.createElement('div');
-                
-                div.id = node_id;
-                div.classList.add('todo_list_div')
-                div.innerHTML = content;
-                button.innerHTML = 'M';
-                button.classList.add('btn_edit');
-                button.value = node_id;
-                button.setAttribute("onclick", `change_input_onclick(${id}, event)`)
-
-                // todo_list.removeChild(todo_list.childNodes[0]);
-                // console.log()
-                // todo_list.removeChild(todo_list.childNodes[1]);
-
+                event_parent.replaceChild(span, secondNode);
+                event_parent.removeChild(thirdNode);
+                // event_parent.replaceChild(M_button, secondNode)
 
 
             } else {
@@ -145,6 +156,7 @@ function edit_success_onclick(id, event) {
 
 }
 
+let test_event;
 
 function change_input_onclick(id, event) {
     test_event = event;
@@ -152,45 +164,30 @@ function change_input_onclick(id, event) {
     // 1. 해당 노드의 정보 받기.
     // 2. 그 노드를 인풋 텍스트로 바꾸기
     // 3. 버튼도 이제 수정완료로 바꾸기. !! 온클릭 이벤트 제대로 연결줘야함
-    const node_id = event.target.value;
-    console.log(node_id)
-    const node_list = document.querySelectorAll('.todo_list_li');
-    // console.log(node_list)
-    const list_div = document.querySelectorAll('.todo_list_div')[node_id];
-    // console.log(list_div)
-    const todo_list = document.querySelectorAll('.todo_list_li')[node_id];
-    // console.log(todo_list);
-    const new_input = document.createElement('input');
-    const button_2 = document.createElement('button');
-
-    const button = document.querySelectorAll('.btn_edit')[node_id - 1];
-    console.log(button);
+    let Edit_button = document.createElement('button');
+    let New_input = document.createElement('input');
+    let event_parent = event.target.parentElement;
+    let event_child = event_parent.children;
+    let firstNode = event_parent.children[0];
+    let secondNode = event_parent.children[1];
+    let thirdNode = event_parent.children[2];
 
 
+
+
+    console.log(event_child);
+    Edit_button.innerText = '수정!'
+    Edit_button.setAttribute("onclick", `edit_success_onclick(${id}, event)`);
+
+    New_input.classList.add('new_input');
+    New_input.classList.add('todo_list_li');
+    event_parent.replaceChild(New_input ,secondNode);
+    event_parent.replaceChild(Edit_button, thirdNode);
+    event_parent.appendChild(thirdNode);
     
-    new_input.type = 'text';
-    new_input.placeholder = list_div.innerHTML;
-    new_input.id = node_id ;
-    new_input.classList.add('new_input');
-    new_input.classList.add('todo_list_div');
-    button_2.type = 'button';
-    button_2.innerText = '수정!';
-    button_2.value = node_id;
-    button_2.classList.add('new_button');
-    button_2.setAttribute("onclick", `edit_success_onclick(${id}, event)`)
-
-    
-
-    console.log(button_2);
-    todo_list.replaceChild(new_input, list_div);
-    todo_list.replaceChild(button_2, button);
-    // todo_list.removeChild(list_div);
-    // todo_list.removeChild(button);
-    // todo_list.removeChild(button_1);
-    // todo_list.appendChild(new_input);
-    // todo_list.appendChild(button_2);
-    // todo_list.appendChild(button_1);
-
+    // event_parent.removeChild(secondNode);
+    // event_parent.appendChild(Edit_button);
+    // event_parent.prepend(New_input);
 
 
 
@@ -200,7 +197,6 @@ function change_input_onclick(id, event) {
         }
     });
 
-    // input text change
 }
 
 
@@ -225,8 +221,8 @@ function delete_onclick(id, event) {
         .then(function(response) {
             let result = response.data['is_success']
             if (result === true) {
-                const removingOne = event.target.parentElement;
-                removingOne.remove();
+                let dd = event.target.parentElement;
+                dd.remove();
 
             } else {
                 alert('fail')
@@ -236,3 +232,34 @@ function delete_onclick(id, event) {
             console.log(error)
         })
 }
+
+
+
+// function done_onclick(id, event) {
+//     // 1. 해당 노드의 정보 받기
+//     // 2. 비동기 통신 보내기
+//     // 3. 성공하면 해당 노드 회색으로.
+//     //4. 실패하면 안되게 하기
+//     axios({
+//             url: `/todos/done/`,
+//             method: 'post',
+//             headers: {
+//                 "content-type": "application/json",
+//                 'X-CSRFToken': getCsrfToken(),
+//             },
+//             data: {
+//                 'id': id,
+//             },
+//         })
+//         .then(function(response) {
+//             let result = response.data['is_success']
+//             if (result === true) {
+                
+//             } else {
+//                 alert('fail')
+//             }
+//         })
+//         .catch((error) => {
+//             console.log(error)
+//         })
+// }
